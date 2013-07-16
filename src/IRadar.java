8ef8bfb7dbf58e70d768db1f77db8b6e71842f7a
striges <striges@ufl.edu>
2013-07-15 22:29:38 -0400

    Refactor radar to interface

---
+++ src/IRadar.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: sugar
+ * Date: 7/15/13
+ * Time: 8:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IRadar {
+}
