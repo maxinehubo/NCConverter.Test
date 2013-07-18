b30b2af1a9852a1588f5061a43d7b501e4353809
striges <striges@ufl.edu>
2013-07-18 15:20:04 +0000

    Add some equation from radar Add beam height calculation to Radar

---
+++ src/NexradEquation.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: sugar
+ * Date: 7/18/13
+ * Time: 2:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NexradEquation {
+}
